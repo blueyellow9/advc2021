66f1c4810be407d57b581802c36e9790a49ea221
blueyellow9 <95772909+blueyellow9@users.noreply.github.com>
2021-12-10 09:27:21 +0000

    Advent of Code 2021, Day 10, Syntax Scoring
    Updated Comments

--- advc10.py
+++ advc10.py
@@ -14,10 +14,15 @@ def main():
         arr = []
         error = 0
         while (len(item)> 0 and error == 0):
+            #Pop the first character from line
             l = item[0]
             item = item[1:]
+            #if it is a open parantheses, push to array
             if l in ['(','[','{','<']:
                 arr.append(l)
+            #else if it is a closed parantheses pop an entry from array,
+            #then compare that both paraentheses match (), [], {}, <>
+            #if dont match flag error and exit after computing the points
             elif(l in [')', '}', ']', '>']):
                 t = arr.pop()
                 if l == ')':
@@ -36,9 +41,13 @@ def main():
                     if t in ['(', '[', '{']:
                         points = points + 25137
                         error = 1
+        #if this point is reached without error flag the array should be 
+        #either empty or an incomplete string
         if (error == 0 and len(arr) > 0):
             completion = ''
             comp_points = 0
+            #for the incomplete string, for each entry from the end of string
+            #compute the matching parantheses score
             for l in reversed(arr):
                 if l == '(':
                     comp_points = comp_points*5 + 1
@@ -51,7 +60,9 @@ def main():
             comp.append(comp_points)
     comp.sort()
     mid = int((len(comp)+1)/2)
+    #Solution to part1
     print(points)
+    #Solution to part2
     print(comp[mid-1])
 
     
